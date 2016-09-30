81b486a337b7c2840ab71aad31f9f579545d7428
shrebox <shreyash15097@iiitd.ac.in>
2016-09-30 19:44:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -52,3 +52,23 @@ button.onclick = function(){
     
 };
 
+// Submit name
+
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+  
+  //Make a request to the server and send the name
+  //Capture the list of the names and render it as a list.
+  var names = ['name1','name2','name3','name4'];
+  var list ='';
+  for(var i=0; i<name.length;i++)
+  {
+      list+= '<li>' + names[i] + '</li>';
+  }
+  
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+};
+
