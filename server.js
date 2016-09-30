c01a7129a8eaf68257acf0afd30d4b584cfcd80e
shrebox <shreyash15097@iiitd.ac.in>
2016-09-30 20:00:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,6 +97,16 @@ app.get('/counter', function(req,res){
    res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function(req,res) ///submit-name?name=xxxx 
+{
+    var name = req.query.name;
+    
+    names.push(name);
+    
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName', function (req, res){
     
     var articleName = req.params.articleName;
@@ -116,15 +126,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names = [];
-app.get('/submit-name/:name', function(req,res)
-{
-    var name = req.params.name;
-    
-    names.push(name);
-    
-    res.send(JSON.stringify(names));
-});
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
