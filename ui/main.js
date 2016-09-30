7f483fb7d81686293a60c736dd179714db48b16c
shrebox <shreyash15097@iiitd.ac.in>
2016-09-30 19:17:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
-var b = document.getElementById('counter'); 
+var button = document.getElementById('counter'); 
 var counter = 0;
-b.onclick = function()
+button.onclick = function()
 {
     counter = counter + 1;
     var span = document.getElementById('count');
