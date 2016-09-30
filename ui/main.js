25900114afc1b3f5247266d94ee780ff293e07f8
shrebox <shreyash15097@iiitd.ac.in>
2016-09-30 18:12:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,15 @@ element.innerHTML = 'Hey edited text!';
 // move the image
 
 var img = document.getElementById('madi');
+
+var maginLeft = 0;
+
+function moveRight(){
+     marginLeft = marginLeft + 10;
+     img.style.marginLeft = marginLeft + 'px';
+}
+
 img.onclick  = function () {
-   img.style.marginLeft = '100px'; 
+   
+   var interval = setInterval(moveRight,100);
 } ;
