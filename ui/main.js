ec2689bcd57c65083256b8b2515adf61c8c32feb
shrebox <shreyash15097@iiitd.ac.in>
2016-09-30 20:04:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -58,17 +58,42 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
+    
+    // create request
+    var request = new XMLHttpRequest();
+    
+    // Capture the request and store it in a variable
+    request.onreadystatechange = function(){
+      
+      if(request.readyState === XMLHttpRequest.DONE){
+          
+          if(request.status === 200)
+          {
+             //Make a request to the server and send the name
+             //Capture the list of the names and render it as a list.
+             
+            var names = request.responseText;
+            names = JSON.parse(names);
+            
+            var list ='';
+            for(var i=0; i<names.length;i++)
+            {
+              list+= '<li>' + names[i] + '</li>';
+            }
+            
+            var ul = document.getElementById('namelist');
+            ul.innerHTML = list;
+            
+          }
+      }
+        
+    };
+    
+    // Make request
+    request.open('GET', 'http://shrebox.imad.hasura-app.io/counter',true);
+    request.send(null);
   
-  //Make a request to the server and send the name
-  //Capture the list of the names and render it as a list.
-  var names = ['name1','name2','name3','name4'];
-  var list ='';
-  for(var i=0; i<names.length;i++)
-  {
-      list+= '<li>' + names[i] + '</li>';
-  }
   
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+  
 };
 
